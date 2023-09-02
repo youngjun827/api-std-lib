e67c9956972a735e6612a0c8534053936479339c
youngjun827 <youngjunjoo827@gmail.com>
2023-09-02 18:21:13 -0400

    feat(ratelimit): implement API rate limiting
    - Added a new file named `ratelimiter.go` to the `utility` package.
    - Implemented a rate limiting middleware function `RateLimiter` that restricts the number of API calls a user can make within a given time frame using an in-memory map.
    - The rate limiting settings include a `rateLimitingWindow` (time frame) and a `requestLimit` (maximum number of requests).
    - The `requests` map stores request counts for each client's IP address.
    - The middleware increments the request count for a client, checks if the limit is exceeded, and schedules a decrement after the time window.
    - Updated `routes.go` to include the rate limiting middleware for the `/user/` endpoint using `utility.RateLimiter`.
    
    This commit introduces API rate limiting to improve performance and add a layer of security against abuse. The middleware ensures fair distribution of resources and prevents overload by limiting the number of requests a user can make within a specified time frame.

--- routes/routes.go
+++ routes/routes.go
@@ -4,30 +4,35 @@ import (
 	"net/http"
 
 	"github.com/youngjun827/api-std-lib/controllers"
+	"github.com/youngjun827/api-std-lib/utility"
 )
 
 func SetupRoutes() {
 	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case "GET":
-			controllers.ListUsers(w, r)
-		case "POST":
-			controllers.CreateUser(w, r)
-		default:
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		}
+		utility.RateLimiter(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			switch r.Method {
+			case "GET":
+				controllers.ListUsers(w, r)
+			case "POST":
+				controllers.CreateUser(w, r)
+			default:
+				http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			}
+		})).ServeHTTP(w, r)
 	})
 
 	http.HandleFunc("/user/", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case "GET":
-			controllers.GetUser(w, r)
-		case "PUT":
-			controllers.UpdateUser(w, r)
-		case "DELETE":
-			controllers.DeleteUser(w, r)
-		default:
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		}
+		utility.RateLimiter(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			switch r.Method {
+			case "GET":
+				controllers.GetUser(w, r)
+			case "PUT":
+				controllers.UpdateUser(w, r)
+			case "DELETE":
+				controllers.DeleteUser(w, r)
+			default:
+				http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			}
+		})).ServeHTTP(w, r)
 	})
 }
---
+++ utility/ratelimiter.go
@@ -0,0 +1,40 @@
+package utility
+
+import (
+	"net/http"
+	"sync"
+	"time"
+)
+
+var (
+	rateLimitingWindow = 1 * time.Minute
+	requestLimit       = 10
+	requests           = make(map[string]int)
+	mu                 sync.Mutex
+)
+
+func RateLimiter(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mu.Lock()
+		defer mu.Unlock()
+
+		clientIP := r.RemoteAddr
+		if count, exists := requests[clientIP]; exists && count >= requestLimit {
+			http.Error(w, "Rate limit exceeded", http.StatusTooManyRequests)
+			return
+		}
+
+		// Increment the request count for this IP
+		requests[clientIP]++
+
+		// Schedule to decrement the count after the rate limiting window
+		go func(clientIP string) {
+			time.Sleep(rateLimitingWindow)
+			mu.Lock()
+			requests[clientIP]--
+			mu.Unlock()
+		}(clientIP)
+
+		next.ServeHTTP(w, r)
+	})
+}
