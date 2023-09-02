fad64e8a00f36bfbe4f53541f339b60726e289d6
youngjun827 <youngjunjoo827@gmail.com>
2023-09-02 09:57:54 -0400

    feat(database): set up database connection and introduce User model
    Created db.go in the db directory to handle database connections.
    Set up the database connection using PostgreSQL with lib/pq.
    Implemented basic error handling for database connection.
    Defined a basic User model structure in models/user.go.
    This commit establishes the foundation for database interactions by providing a way to initialize and manage the database connection. The User model structure has been introduced, representing the basic attributes of a user. Additionally, the main.go file has been updated to initialize the database connection when the API is started.
    
    Note: In real-world applications, secure practices should be followed for handling credentials and sensitive information, and passwords should be securely hashed.

---
+++ db/db.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "your_username"
+	password = "your_password"
+	dbname   = "api-std-lib-db"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	var err error
+	DB, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+
+	fmt.Println("Successfully connected to database.")
+}
--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/youngjun827/api-std-lib/db"
 )
 
 func main() {
+	db.InitDB()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	})
