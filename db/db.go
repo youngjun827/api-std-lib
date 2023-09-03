c1f858d8b02df8fc51be844e152f1d0ffb5be2ae
youngjun827 <youngjunjoo827@gmail.com>
2023-09-03 19:51:41 -0400

    feat(db): load database credentials from .env
    Replace hardcoded database credentials with loading them from the .env
    file. This change allows for a more flexible and secure configuration
    of the database connection parameters.

--- db/db.go
+++ db/db.go
@@ -1,26 +1,32 @@
 package db
 
 import (
+	"bufio"
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 	"time"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "your_username"
-	password = "your_password"
-	dbname   = "api-std-lib-db"
-)
-
 var DB *sql.DB
 
 func InitDB() {
-	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	errEnv := loadEnvVariables()
+	if errEnv != nil {
+		log.Fatalf("Error loading .env file: %v", errEnv)
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
 	var err error
 	DB, err = sql.Open("postgres", connStr)
@@ -38,5 +44,33 @@ func InitDB() {
 		log.Fatalf("Failed to ping database: %v", err)
 	}
 
-	fmt.Println("Successfully connected to database.")
+	fmt.Println("Successfully connected to the database.")
+}
+
+func loadEnvVariables() error {
+	// Open the .env file.
+	file, err := os.Open(".env")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Read the file line by line.
+	lines := make([]string, 0)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	// Parse and set the environment variables.
+	for _, line := range lines {
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) == 2 {
+			key := strings.TrimSpace(parts[0])
+			value := strings.TrimSpace(parts[1])
+			os.Setenv(key, value)
+		}
+	}
+
+	return nil
 }
