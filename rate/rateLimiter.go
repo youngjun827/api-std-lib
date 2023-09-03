d988589bf6ebb23d2e938dd729125e2f1d7e707c
youngjun827 <youngjunjoo827@gmail.com>
2023-09-03 13:10:10 -0400

    refactor(rate-limiter): optimize rate limiter using sync.Map
    This commit optimizes the rate limiter for parallel processing and eliminates the global mutex by using Go's built-in sync.Map to store request counts per client IP. The RateLimiter function has been modified to use sync.Map for concurrent read and write access, ensuring safe and efficient rate limiting.

--- rate/rateLimiter.go
+++ rate/rateLimiter.go
@@ -9,30 +9,27 @@ import (
 var (
 	rateLimitingWindow = 1 * time.Minute
 	requestLimit       = 10
-	requests           = make(map[string]int)
-	mu                 sync.Mutex
+	requests           = sync.Map{}
 )
 
 func RateLimiter(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		mu.Lock()
-		defer mu.Unlock()
-
 		clientIP := r.RemoteAddr
-		if count, exists := requests[clientIP]; exists && count >= requestLimit {
+
+		count, _ := requests.LoadOrStore(clientIP, 0)
+		requestCount := count.(int)
+
+		if requestCount >= requestLimit {
 			http.Error(w, "Rate limit exceeded", http.StatusTooManyRequests)
 			return
 		}
 
-		// Increment the request count for this IP
-		requests[clientIP]++
+		requests.Store(clientIP, requestCount+1)
 
-		// Schedule to decrement the count after the rate limiting window
 		go func(clientIP string) {
 			time.Sleep(rateLimitingWindow)
-			mu.Lock()
-			requests[clientIP]--
-			mu.Unlock()
+
+			requests.LoadAndDelete(clientIP)
 		}(clientIP)
 
 		next.ServeHTTP(w, r)
